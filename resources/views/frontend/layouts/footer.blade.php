e1fd81c14d819b03cca058602674108ded3ae55b
shankar <shankarsaini348@gmail.com>
2024-09-23 21:15:47 +0530

    leatet

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <p>Movie Maker Copyright © 2024.</p>
+      </div>
+
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Movie Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<!-- Header -->
+<header>
+  <div class="container">
+    <!-- For Desktop -->
+<nav class="navbar navbar-expand-sm for_desktop">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="/assets/images/logo.jpg" alt="logo">
+    </a>
+    <div class="ml-auto">
+      <form class="d-flex">
+        <input class="form-control me-2" type="text" placeholder="What are you looking for?">
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- For Mobile Version -->
+<nav class="navbar navbar-expand-sm for_mobile">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./images/logo.jpg" alt="logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a href="#" >18+</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Bollywood</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Dual Audio</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Hollywood</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Horror</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Punjabi</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Biography</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Mystery</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >South</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Hindi Dubbed</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Documentary</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Movie Series Collections</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Web Series</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Action</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Advanture</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" >Family</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="text" placeholder="What are you looking for?">
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+</header>
